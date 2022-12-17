40dc111753bb250056d288d50d84ec90025ed027
Eliabe Ribeiro Mota <eliaberibeiro06@hotmail.com>
2022-12-17 11:55:48 -0300

    JS - Implementada operação fatorial(17/12)

--- assets/js/calculadora.js
+++ assets/js/calculadora.js
@@ -40,16 +40,82 @@ function teclado(botao) {
 }
 
 function limpar() {
+  document.querySelector("#txtValorA").value = "";
+  document.querySelector("#txtValorB").value = "";
   document.querySelector("#visor").value = "";
 }
 
 function backspace() {
   var visor = document.querySelector("#visor");
   if (!visor.value == "") {
-    var array = [visor.value]
+    var array = [visor.value];
     console.log(array);
     array.splice(0, 1);
-    document.querySelector("#visor").value = array
+    document.querySelector("#visor").value = array;
     console.log(array);
+  }
+}
+
+function formatarValor(valor) {
+  return valor.toLocaleString("pt-BR", { minimumFractionDigits: 2 });
+}
+
+function calcular(operacao, valorB = 1) {
+  alert("entrou no calcular");
+  let valorA = parseFloat(document.getElementById("txtValorA").value);
+  var status = true;
+  //let valorB = parseFloat(document.getElementById("txtValorB").value);
+
+  let resultado;
+
+  switch (operacao) {
+    case "+":
+      resultado = valorA + valorB;
+      alert(resultado);
+      break;
+
+    case "-":
+      resultado = valorA - valorB;
+      alert(resultado);
+      break;
+
+    case "/":
+      resultado = valorA / valorB;
+      alert(resultado);
+      break;
+
+    case "*":
+      resultado = valorA * valorB;
+      alert(resultado);
+      break;
+    case "quadrado":
+      resultado = Math.sqrt(valorA);
+      break;
+    case "potencia":
+      resultado = Math.pow(valorA, 2);
+      break;
+    case "fatorial":
+      var fatorial = 1;
+      for (i = valorA; i > 1; i--) {
+        fatorial *= i;
+      }
+      resultado = fatorial;
+      break;
+    default:
+      alert("Ocorreu um erro ao calcular!");
+      var status = false;
+  }
+
+  if (status == true) {
+    alert("Calculo realizdo com sucesso");
+
+    // Formatando o valor para ter: separação a cada 3 dígitos, vírgula
+    let resultado_formatado = formatarValor(resultado);
+
+    alert(resultado_formatado);
+
+    document.getElementById("visor").value = resultado_formatado;
+  } else {
+    alert("Ocorreu um erro ao calcular!");
   }
 }
